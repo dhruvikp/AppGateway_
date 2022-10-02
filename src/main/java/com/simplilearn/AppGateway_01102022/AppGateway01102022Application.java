0b7883ce70a75e1f538e119856cd3d1e8bada544
Dhruvik <dhruviksparikh@gmail.com>
2022-10-02 12:17:10 +0530

    Added changes

---
+++ src/main/java/com/simplilearn/AppGateway_01102022/AppGateway01102022Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.AppGateway_01102022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppGateway01102022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppGateway01102022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/AppGateway_01102022/AppGateway01102022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.AppGateway_01102022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppGateway01102022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
